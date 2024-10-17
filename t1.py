22907c87d17da65495046c8a5781fc5e389ecf13
Caio <120686918+Szerman08@users.noreply.github.com>
2024-10-16 21:36:00 -0300

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox, Tk
+
+app = Tk()
+app.title("App Szerman")
+app.geometry("500x300")
+
+messagebox.showinfo(title= "App Szeman", message="Curso do Caio")
+
+app.mainloop()
+
+
